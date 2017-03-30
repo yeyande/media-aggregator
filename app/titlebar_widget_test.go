bd798fbba8a48535e5869734dcffd0d9c86cf577
Kaleb Smart <kalebsma@gmail.com>
2017-03-29 22:01:33 -0500

    Cleaned up test

--- app/titlebar_widget_test.go
+++ app/titlebar_widget_test.go
@@ -2,7 +2,6 @@ package app
 
 import "testing"
 import "reflect"
-import "github.com/jroimartin/gocui"
 
 /**
   * A new menu widget should be placed at the same height of the title bar and
@@ -61,12 +60,6 @@ func TestAddMenuWidget(t *testing.T) {
     }
 }
 
-type mockGui struct { gocui.Gui }
-
-func (g *mockGui) View(name string) (*gocui.View, error) {
-    return &gocui.View{}, nil
-}
-
 func TestShiftSelectedEntry(t *testing.T) {
     w := Widget{"", 0, 0, 0, 0, "", false}
     menuWidget := *createMenuWidget("", 0, 0, 0, 0, "")
@@ -75,11 +68,6 @@ func TestShiftSelectedEntry(t *testing.T) {
         in int
         want int
     }{
-        /*
-        Test Cases:
-        Incrementing a list at the end
-        Decrementing a list at the end
-        */
         {titlebarWidget{w, 0, []MenuWidget{}}, 1, 0},
         {titlebarWidget{w, 0, []MenuWidget{}}, -1, 0},
         {titlebarWidget{w, 0, []MenuWidget{menuWidget}}, 1, 0},
@@ -95,21 +83,11 @@ func TestShiftSelectedEntry(t *testing.T) {
         c.title.shiftSelectedEntry(c.in)
         got := c.title.selectedEntry
         if c.want != got {
-            t.Errorf("TitlebarWidget.shiftSelectedEntry(%d) with %d entries should cause title.selectedEntry = %d, got %d",
+            t.Errorf("title.shiftSelectedEntry(%d) with %d entries should " +
+                        "cause title.selectedEntry = %d, got %d",
                     c.in, len(c.title.menuEntries), c.want, got)
         }
     }
-    /*
-    title := titlebarWidget{
-        Widget{"title", 0, 0, 20, 2, "title", false},
-        0,
-        []MenuWidget{
-            *createMenuWidget("foo", 0, 0, 3, 0, "foo"),
-            *createMenuWidget("bar", 0, 0, 3, 0, "bar"),
-        },
-    }
-    title.selectLeft(&mockGui{}, nil)
-    */
 }
 
 func createMenuWidget(name string, x, y, w, h int, body string) *MenuWidget {
